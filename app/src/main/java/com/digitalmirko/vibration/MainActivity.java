6e6e834765f416fccb46d148a7c450ea5ff09b9b
Mirko Cukich <mirkocukich@gmail.com>
2017-02-10 10:53:53 -0600

    Initial commit

---
+++ app/src/main/java/com/digitalmirko/vibration/MainActivity.java
@@ -0,0 +1,13 @@
+package com.digitalmirko.vibration;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
